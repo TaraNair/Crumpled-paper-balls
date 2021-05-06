d9fde369d3d498061a1e49420a13b5119b19d9f3
TaraNair <64844272+TaraNair@users.noreply.github.com>
2021-05-06 19:52:04 +0530

    Add files via upload

---
+++ Dustbin.js
@@ -0,0 +1,30 @@
+class Dustbin{
+    constructor(x, y, w, h){
+        var options = {
+            'isStatic':true,
+        }
+        this.image = loadImage("dustbingreen.png");  
+
+        this.width = w;
+        this.height = h;
+        this.bodyL = Bodies.rectangle(x - 110, y, 20, 100, options);
+        this.bodyR = Bodies.rectangle(x + 110, y, 20, 100, options);
+        this.bodyB = Bodies.rectangle(x, y + 40, 100, 20, options);
+
+        World.add(world, this.bodyL);
+        World.add(world, this.bodyR);
+        World.add(world, this.bodyB);
+    }
+
+    display(){
+        var posL =this.bodyL.position;
+        var posR =this.bodyR.position;
+        var posB =this.bodyB.position;
+        push();
+        fill("purple");
+        imageMode(CENTER);
+        image(this.image, 600, 510, this.width * 2.6, this.height * 15);
+
+        pop();
+    }
+}
